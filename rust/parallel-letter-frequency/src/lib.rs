0dc8589c45009d5c4f1df26653a7101ca38f0b57
FazMataz <samefarrar@gmail.com>
2023-12-27 13:00:14 +0000

    Initial solution

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,11 +1,48 @@
 use std::collections::HashMap;
+use std::thread;
+use unicode_segmentation::UnicodeSegmentation;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    todo!(
-        "Count the frequency of letters in the given input '{input:?}'. Ensure that you are using {} to process the input.",
-        match worker_count {
-            1 => "1 worker".to_string(),
-            _ => format!("{worker_count} workers"),
+
+    if input.is_empty() {
+        return HashMap::new()
+    }
+
+    let input_owned: Vec<String> = input.iter().map(|s| s.to_string()).collect();
+    let chunk_size = (input_owned.len() + worker_count - 1) / worker_count;
+    let mut handles = Vec::with_capacity(worker_count);
+
+    for chunk in input_owned.chunks(chunk_size) {
+        let chunk_cloned = chunk.to_vec();
+        let handle = thread::spawn(move || {
+            chunk_cloned.into_iter().map(|l| count_chars_in(&l)).collect()
+        });
+        handles.push(handle);
+    }
+
+    let mut result = HashMap::new();
+    for handle in handles {
+        let partial_result: Vec<HashMap<char, usize>> = handle.join().unwrap();
+        for hashmap in partial_result {
+            for (key, value) in hashmap {
+                let update_count = result.entry(key).or_insert(0);
+                *update_count += value;
+            }
         }
-    );
+    }
+
+    return result
+}
+
+fn count_chars_in(line: &str) -> HashMap<char, usize> {
+    let mut counts: HashMap<char, usize> = HashMap::new();
+    for g in line.graphemes(true) {
+        for ch in g.to_lowercase().chars() {
+            if ch.is_alphabetic() {
+                let update_count = counts.entry(ch).or_insert(0);
+                *update_count += 1
+            }
+        }
+    }
+    return counts;
 }
