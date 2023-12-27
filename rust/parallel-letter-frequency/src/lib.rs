c696cec3e72ce4d0214be284610c2881420bbf28
FazMataz <samefarrar@gmail.com>
2023-12-27 13:03:22 +0000

    Eliminate graphemes, use Arc and Mutex to do all aggregation in one loop

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,48 +1,41 @@
 use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use std::thread;
-use unicode_segmentation::UnicodeSegmentation;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-
     if input.is_empty() {
-        return HashMap::new()
+        return HashMap::new();
     }
 
     let input_owned: Vec<String> = input.iter().map(|s| s.to_string()).collect();
-    let chunk_size = (input_owned.len() + worker_count - 1) / worker_count;
-    let mut handles = Vec::with_capacity(worker_count);
+
+    let chunk_size = (input.len() + worker_count - 1) / worker_count;
+    let result = Arc::new(Mutex::new(HashMap::new()));
+    let mut handles = Vec::new();
 
     for chunk in input_owned.chunks(chunk_size) {
-        let chunk_cloned = chunk.to_vec();
+        let result = Arc::clone(&result);
+        let chunk = chunk.to_vec();
+
         let handle = thread::spawn(move || {
-            chunk_cloned.into_iter().map(|l| count_chars_in(&l)).collect()
+            let mut local_map = HashMap::new();
+            for line in chunk {
+                for ch in line.chars().filter(|c| c.is_alphabetic()).map(|c| c.to_lowercase().next().unwrap()) {
+                    *local_map.entry(ch).or_insert(0) += 1;
+                }
+            }
+
+            let mut result = result.lock().unwrap();
+            for (key, value) in local_map {
+                *result.entry(key).or_insert(0) += value;
+            }
         });
         handles.push(handle);
     }
 
-    let mut result = HashMap::new();
     for handle in handles {
-        let partial_result: Vec<HashMap<char, usize>> = handle.join().unwrap();
-        for hashmap in partial_result {
-            for (key, value) in hashmap {
-                let update_count = result.entry(key).or_insert(0);
-                *update_count += value;
-            }
-        }
+        handle.join().expect("Thread panicked");
     }
 
-    return result
-}
-
-fn count_chars_in(line: &str) -> HashMap<char, usize> {
-    let mut counts: HashMap<char, usize> = HashMap::new();
-    for g in line.graphemes(true) {
-        for ch in g.to_lowercase().chars() {
-            if ch.is_alphabetic() {
-                let update_count = counts.entry(ch).or_insert(0);
-                *update_count += 1
-            }
-        }
-    }
-    return counts;
+    Arc::try_unwrap(result).unwrap().into_inner().unwrap()
 }
